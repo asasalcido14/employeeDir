549a500a87bcaa747855e986427c21c5de13ff45
Asa Salcido <asasalcido14@email.arizona.edu>
2020-10-11 20:10:01 -0700

    1st commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import Navbar from './componets/Navbar';
+import Wrapper from './componets/Wrapper';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <Wrapper>
+      <Navbar />
+
+    </Wrapper>
+      
+  );
+}
+
+export default App;
